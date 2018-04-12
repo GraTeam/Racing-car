16acf84b774ed58dee1ba2962c254653171c5b2d
Shrouk04 <37943346+Shrouk04@users.noreply.github.com>
2018-04-12 08:46:34 +0200

    make background music
    set it on scenes (when is it play) , add it to main audio mixer

---
+++ Assets/Scripts/BG_Music.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BG_Music : MonoBehaviour {
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (SceneManager.GetActiveScene().name == "RaceArea01")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "RaceArea02")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "RaceArea03")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "RaceArea04")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "RaceArea05")
+        {
+            Destroy(this.gameObject);
+        }
+        if (SceneManager.GetActiveScene().name == "RaceArea06")
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+}
